22ae458c0b0faae8fffa391b7994cba441b89379
Muhammedshifin <muhammedshifink4@gmail.com>
2023-02-05 08:41:32 +0530

    Update accept.py

--- plugins/Don/accept.py
+++ plugins/Don/accept.py
@@ -26,13 +26,20 @@ gif = [
 @Client.on_chat_join_request(filters.group | filters.channel & ~filters.private)
 async def approve(client: Client, message: Message):
     chat=message.chat # Chat
-    add_group(message.chat.id)
+    add_group(chat.id)
     user=message.from_user # User
     print(f"{user.first_name} 𝙹𝙾𝙸𝙽𝙴𝙳 ⚡") # Logs
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     img = random.choice(gif)
-    add_user(message.from.user.id)
+    add_user(user.id)
     #nothingenter
     await client.send_video(user.id,img, "**Hello {}!\nYour Request To Join {} was approved👍\n\n⚠️click /start to see my power Powered By @sinimapremi **".format(message.from_user.mention, message.chat.title))
-#by master
+    if ACC_SND_LOG == "on":
+        await client.send_message(
+            chat_id=LOG_CHANNEL
+            text="*#New_Approval\n\nName: {}\n\nChat: {} \n\n Join @sinimapremi**".format(message.from_user.mention, message.chat.title))
+            
+        
+        
+    
 
