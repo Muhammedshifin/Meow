bab98b2195a03da0a6464e8f4602d8edd92c1375
Muhammedshifin <muhammedshifink4@gmail.com>
2023-02-08 11:49:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Muhammedshifin/Ajax-Extra-Features.git /Ajax-Extra-Features 
+  git clone https://github.com/Muhammedshifin/Meow.git /Meow
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax-Extra-Features 
+  git clone $UPSTREAM_REPO /Meow
 fi
-cd /Ajax-Extra-Features 
+cd /Meow
 pip3 install -U -r requirements.txt
 echo "Starting ᗩᒍᗩ᙭....🔥"
 python3 bot.py
